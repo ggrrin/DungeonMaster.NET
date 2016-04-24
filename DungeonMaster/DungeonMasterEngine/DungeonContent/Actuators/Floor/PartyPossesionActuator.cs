e1d9f37353889008cb94d53ce980a04f7dcf6bc1
ggrrin <petrgeiger0@gmail.com>
2016-04-24 21:26:13 +0100

    Added missing files

---
+++ DungeonMaster/DungeonMasterEngine/DungeonContent/Actuators/Floor/PartyPossesionActuator.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using DungeonMasterEngine.DungeonContent.Items;
+using DungeonMasterEngine.DungeonContent.Tiles;
+using DungeonMasterEngine.Interfaces;
+using DungeonMasterEngine.Player;
+using Microsoft.Xna.Framework;
+
+namespace DungeonMasterEngine.DungeonContent.Actuators.Floor
+{
+    public class PartyPossesionActuator : FloorActuator 
+    {
+        public PartyPossesionActuator(Vector3 position, Tile actuatorTile, Tile targetTile, IConstrain constrain, ActionStateX action) : 
+            base(position, actuatorTile, targetTile, constrain,  action)
+        { }
+
+        protected override void TestAndRun(object enteringObject)
+        {
+            var theron = enteringObject as Theron;
+            if (theron == null)
+            {
+                var items = new[] { theron.Hand }
+                    .Concat(theron.PartyGroup.SelectMany(x => x.Inventory))
+                    .Concat(CurrentTile.SubItems);
+
+                if (items.Any(Constrain.IsAcceptable))
+                    SendMessage();
+            }
+        }
+    }
+}
---
+++ DungeonMaster/DungeonMasterEngine/DungeonContent/Items/IItem.cs
@@ -0,0 +1,12 @@
+﻿using DungeonMasterEngine.DungeonContent.Tiles;
+using DungeonMasterEngine.Interfaces;
+using Microsoft.Xna.Framework;
+
+namespace DungeonMasterEngine.DungeonContent.Items
+{
+    public interface IItem : ILocalizable<Tile>
+    {
+        GrabableItem ExchangeItems(GrabableItem item);
+        void Update(GameTime gameTime);
+    }
+}
